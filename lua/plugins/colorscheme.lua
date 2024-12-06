c33c1c16eb9e000acf88a5008883fa8f8547c0cc
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-12-06 18:16:19 +0100

    colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,44 @@
+return { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup{
+            transparent_background = false,
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                barbar = true,
+                mason = true,
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                        ok = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                        ok = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+            }
+        }
+        vim.cmd.colorscheme("catppuccin")
+    end,
+}
+
