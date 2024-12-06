ee6705cadfa9c4522ecbefac32f201bc3203c108
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-12-06 21:36:19 +0100

    telescope and tree

--- init.lua
+++ init.lua
@@ -9,3 +9,13 @@ vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.o.number = true
 vim.o.relativenumber = false
 
+-- Shortcuts
+local tree = require('nvim-tree.api')
+vim.keymap.set('n', '<leader>e', tree.tree.toggle, { desc = 'Toggle tree' })
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
+-- vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
+-- vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+
